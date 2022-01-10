57dac4b6f1f7a4c08599a07cb6386f550db7a9ba
Zaryab Azeem <zaryabazeem.com>
2022-01-11 01:00:01 +0500

    posting feature add up

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ToDoForms from './ToDoForms';
 import './App.css';
 
 function App() {
- 
+  const [alert, setAlert] = useState(false);
   const [ toDoList, setToDoList]= useState([]);
   useEffect(() => {
     // let mounted = true;
@@ -55,16 +55,38 @@ function App() {
     setToDoList(filtered)
   }
 
+  useEffect(() => {
+    if(alert) {
+      setTimeout(() => {
+        setAlert(false);
+      }, 1000)
+    }
+  }, [alert])
+
   const addtask = (userInput) => {
-    let copy =  [...toDoList];
-    copy = [...copy,{id: toDoList.length +1, task: userInput,complete: false}];
-    setToDoList(copy);
+    // let copy =  [...toDoList];
+    // copy = [...copy,{id: toDoList.length +1, task: userInput,complete: false}];
+    // //setToDoList(copy);
+    // yahan sa film shuru hoi ha//
+    
+        setAlert(true);
+
+    const task=userInput;
+    return fetch('http://localhost:3333/saif', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ task, complete: false })
+    })
+      .then(data => data.json())
   }
 
   return (
     <div className="App">
       <Header />
       <ToDoForms addtask={addtask}/>
+      {alert && <h2> Submit Successful</h2>}
       <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter} />
       
     </div>
